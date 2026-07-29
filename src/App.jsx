6dae1487438c2dd27f19e0220aee54608b39ca02
marlonrod12@gmail.com <247076703+Noctis-arch@users.noreply.github.com>
2026-07-28 20:16:36 -0400

    Implemented reducer actions and connect components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import { useReducer } from "react";
 import "./App.css";
 
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
 const initialState = [];
 
 function reducer(state, action) {
   switch (action.type) {
+    case "ADD_TODO":
+      return [action.payload, ...state];
+
+    case "DELETE_TODO":
+      return state.filter((todo) => todo.id !== action.payload);
+
+    case "TOGGLE_TODO":
+      return state.map((todo) =>
+        todo.id === action.payload
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      );
+
+    case "EDIT_TODO":
+      return state.map((todo) =>
+        todo.id === action.payload.id
+          ? { ...todo, title: action.payload.title }
+          : todo
+      );
+
     default:
       return state;
   }
@@ -16,6 +39,13 @@ function App() {
   return (
     <div className="app">
       <h1>Todo List</h1>
+
+      <TodoForm dispatch={dispatch} />
+
+      <TodoList
+        todos={todos}
+        dispatch={dispatch}
+      />
     </div>
   );
 }
